c4241b8f22ea36af8df36b0c7a92e68a638ace76
LawrenceWilliams00 <lawriewilliams@gmail.com>
2023-03-11 11:48:41 +1300

    added get from websocket

--- hackathon-frontend/src/components/InputOutput.tsx
+++ hackathon-frontend/src/components/InputOutput.tsx
@@ -1,19 +1,43 @@
-import React from "react";
 import { IReceiveData } from "./types";
 
-export const getData = (): IReceiveData => {
-  const test: IReceiveData = {
-    roundNumber: 4,
-    drinks: [
-      { drinkName: "Coke", drinkChance: 10 },
-      { drinkName: "vanilla essence", drinkChance: 9 },
-      { drinkName: "ice", drinkChance: 24 },
-    ],
-    isFinished: false,
-    addedDrinks: ["tobasco", "mayonnaise", "vodka", "apple juice"],
+interface IGetData {
+    current: IReceiveData | undefined;
+    setCurrent: React.Dispatch<React.SetStateAction<IReceiveData | undefined>>;
+}
+
+
+export const getData = ({ current, setCurrent }: IGetData) => {
+    const ws = new WebSocket("ws://3.25.151.51:3000");
+  
+    const test: IReceiveData = {
+      roundNumber: 4,
+      drinks: [
+        { drinkName: "Coke", drinkChance: 10 },
+        { drinkName: "vanilla essence", drinkChance: 9 },
+        { drinkName: "ice", drinkChance: 24 },
+      ],
+      isFinished: false,
+      addedDrinks: ["tobasco", "mayonnaise", "vodka", "apple juice"],
+    };
+  
+    ws.onopen = () => {
+      console.log("WebSocket connection established");
+    };
+  
+    ws.onmessage = (event) => {
+      const newData: IReceiveData = JSON.parse(event.data);
+      console.log("Received data from WebSocket:", newData);
+      console.log("New Data:" , newData)
+      setCurrent(newData || test);
+    };
+  
+    ws.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+  
+    // Return a function that closes the WebSocket connection when the component unmounts
+    return () => {
+        console.log("Function Finished")
+      ws.close();
+    };
   };
-
-  return test;
-};
-
-export const sendData = () => {};
