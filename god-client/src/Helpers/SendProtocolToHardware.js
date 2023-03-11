54f78a28c494e21227bd47791f6089b1f2b0e4c3
Rachel Meadows <rachelnewzealand@gmail.com>
2023-03-11 23:14:22 +1300

    Add error handling to serialPort connection

--- god-client/src/Helpers/SendProtocolToHardware.js
+++ god-client/src/Helpers/SendProtocolToHardware.js
@@ -1,10 +1,17 @@
 import { SerialPort } from 'serialport';
+const serialPort = new SerialPort({ path: 'COM5', baudRate: 9600 }, false);
+serialPort.on('error', function (e) {
+  console.error('Failed to send protocol to robot.', e);
+});
+
 function SendProtocolToHardware(voteResult = '') {
   try {
-    const serialport = new SerialPort({ path: 'COM5', baudRate: 9600 }, false);
-    serialport.write(voteResult);
+    serialPort.write(voteResult);
   } catch (e) {
-    console.error('Failed to send protocol to robot.', e);
+    console.error(
+      'Failed to send protocol to robot.',
+      `${e}\n(End of error report; the program is still running).`
+    );
   }
 }
 
