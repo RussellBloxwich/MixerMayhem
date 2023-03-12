a259afcccfaee4c68c80a7d45652d2d4552b9671
Aden J <adenajowsey@gmail.com>
2023-03-12 14:52:12 +1300

    protocol map

--- god-client/src/Helpers/SendProtocolToHardware.js
+++ god-client/src/Helpers/SendProtocolToHardware.js
@@ -4,9 +4,11 @@ serialPort.on('error', function (e) {
   console.error('Failed to send protocol to robot.', e);
 });
 
-export function SendProtocolToHardware(voteResult = '') {
+export function SendProtocolToHardware(voteResult) {
+  const result = stringMap.find(inputObject => inputObject.inputName === voteResult)?.outputName;
+  if (result == undefined) {return console.log(`Error: Failed to find protocol output`)};
   try {
-    serialPort.write(voteResult);
+    serialPort.write(result);
   } catch (e) {
     console.error(
       'Failed to send protocol to robot.',
@@ -14,5 +16,18 @@ export function SendProtocolToHardware(voteResult = '') {
     );
   }
 }
+
+const stringMap = [
+  {'inputName': 'Coke', 'outputName': 'l1'},
+  {'inputName': 'Milk', 'outputName': 'l2'},
+  {'inputName': 'Juice', 'outputName': 'l3'},
+  {'inputName': 'Lemon', 'outputName': 'p1'},
+  {'inputName': 'Peppermint', 'outputName': 'p2'},
+  {'inputName': 'Soy', 'outputName': 'p3'},
+  {'inputName': 'Tabasco', 'outputName': 'p4'},
+  {'inputName': 'Heat', 'outputName': 'heating'},
+  {'inputName': 'Mix', 'outputName': 'spin'},
+  {'inputName': 'Finish', 'outputName': 'smoke'},
+];
 
 export default SendProtocolToHardware;
